b7ef048d1ee2dd5bf5ce4c21f38b3ff5a2ee3651
Mano Araujo <manoaraujo@gmail.com>
2023-07-12 13:11:27 -0300

    products-responsive

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// const botaoIniciarSecao = document.querySelector(["data-header-button"]);
+
+// botaoIniciarSecao.addEventListener("click", () => {
+//    window.location.href = "../html/home-login.html";
+// });
+
+function teste() {
+   alert("teste");
+}
