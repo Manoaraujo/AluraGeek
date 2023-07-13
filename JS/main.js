3026f164c4c1de26dc01274fc19a00de55f94c58
Mano Araujo <manoaraujo@gmail.com>
2023-07-13 10:58:56 -0300

    update archives

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
-const botaoIniciarSecao = document.querySelector("[data-header-button]");
+// const loginButton = document.querySelector("[data-header-button]");
 
-botaoIniciarSecao.addEventListener("click", () => {
-   // alert("teste");
-   window.location.href = "./home-login.html";
+// loginButton.addEventListener("click", () => {
+//    window.location.href = "./home-login.html";
+// });
+
+const addItemButton = document.querySelector(".products__button-add");
+
+addItemButton.addEventListener("click", () => {
+   console.log("test");
+   window.location.href = "./products-add.html";
 });
