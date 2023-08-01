9593085101038a5b75adb80b799197e1e8b3b1cd
Mano Araujo <manoaraujo@gmail.com>
2023-07-31 21:42:34 -0300

    corrections

--- JS/controllers/go-product-page.js
+++ JS/controllers/go-product-page.js
@@ -1,16 +1,13 @@
 function showDetailedProducts(event) {
-   
    const product = event.target.closest("[data-product-id]");
-   
+
    if (product) {
       event.preventDefault();
       const selectedProduct = product.parentNode.getAttribute("data-id");
-      const productUrl = `../../html/products-description.html?id=${selectedProduct}`;
-      
+      const productUrl = `html/products-description.html?id=${selectedProduct}`;
+
       window.location.href = productUrl;
    }
 }
 
 document.addEventListener("click", showDetailedProducts);
-
-
